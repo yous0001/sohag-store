7d72d78e5bc548d18f265571a81a7d805f4a3ad2
Yousef emad mikhail <mad33833@gmail.com>
2025-03-23 16:34:22 +0200

    add:category model

---
+++ DB/models/category.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const {Schema, model}=mongoose
+
+const categorySchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        },
+    slug:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+    },
+    Images:[{
+        secure_url:{type:String, required:true},
+        public_id:{type:String, required:true} 
+    }],
+    createdBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:false //only now until building user model
+    }
+    
+},{timestamps:true})
+
+export const Category= mongoose.models.Category || model("Category", categorySchema) 
