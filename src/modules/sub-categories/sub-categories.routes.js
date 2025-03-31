f9e43af25750855b8f0ae25f05ef8d21a71fa2ef
Yousef emad mikhail <mad33833@gmail.com>
2025-03-31 19:22:15 +0200

    add:subcategory router

---
+++ src/modules/sub-categories/sub-categories.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express"
+import * as subCategoryController from "./sub-categories.controller.js"
+import expressAsyncHandler from "express-async-handler";
+
+
+const subCategoryRouter=Router()
+
+export {subCategoryRouter}
